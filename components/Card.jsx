15112e1a4eb8dc7cdb4ca30efd6c3993ef1a2c0e
yjb23tech <yjb23tech@gmail.com>
2023-12-01 15:49:53 +0000

    Syntax sanity check

---
+++ components/Card.jsx
@@ -0,0 +1,12 @@
+import album_art from "../images/album_cover_1.png"
+import album_icon from "../images/album_icon.png"
+import artist_icon from "../images/artist_icon.png"
+import genre_icon from "../images/genre_icon.png"
+
+const Card = () => {
+    return (
+        <div>
+            Placeholder 
+        </div>
+    )
+}
