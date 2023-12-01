511ac4b2f6383decbc3dd5cea039bcc165d972ba
yjb23tech <yjb23tech@gmail.com>
2023-12-01 18:29:36 +0000

    lots of experimentation

--- App.jsx
+++ App.jsx
@@ -7,10 +7,13 @@ const App = () => {
         <div>
             <h1>Above It All</h1>
             <Card 
-                image_art = "album_cover_2.png" 
+                image_art = "album_cover_1.png" 
                 main_title="YEEZUS" 
-                artist_name="Kanye West" 
+                album_icon="album_icon.png"
                 album_name="TurboGrafx" 
+                artist_icon="artist_icon.png"
+                artist_name="Kanye West" 
+                genre_icon="genre_icon.png"
                 genre_name="Sophisticated Ignorance"
             />
         </div>
--- components/Card.jsx
+++ components/Card.jsx
@@ -1,22 +1,18 @@
-import album_icon from "../public/images/album_icon.png"
-import artist_icon from "../public/images/artist_icon.png"
-import genre_icon from "../public/images/genre_icon.png"
-
 const Card = (props) => {
     return (
         <div className="album-card">
             <img src={`images/${props.image_art}`} />
             <h3>{props.main_title}</h3>
             <div className="info-group">
-                <img src={album_icon} />
+                <img src={`images/${props.album_icon}`} />
                 <p>{props.album_name}</p>
             </div>
             <div className="info-group">
-                <img src={artist_icon} />
+                <img src={`images/${props.artist_icon}`} />
                 <p>{props.artist_name}</p>
             </div>
             <div className="info-group">
-                <img src={genre_icon} />
+                <img src={`images/${props.genre_icon}`} />
                 <p>{props.genre_name}</p>
             </div>
         </div>
